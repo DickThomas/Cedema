f90625335ff2b6fb9ed7e946d6b3a51bb2cb0504
Dick Thomas <dthomas@speaksthegeek.co.uk>
2018-07-05 21:03:19 +0100

    updated java

--- plugins/jdk.plugin/install.sh
+++ plugins/jdk.plugin/install.sh
@@ -11,15 +11,10 @@ wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://downl
 mkdir -p "/opt/jdk"
 	tar xvf jdk-8u121-linux-x64.tar.gz -C /opt/jdk/
 
-else
-     wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/10.0.1+10/fb4372174a714e6b8c52526dc134031e/jdk-10.0.1_linux-i586_bin.tar.gz
-    mkdir -p "/opt/jdk"
-    tar xvf jdk-8u121-linux-i586.tar.gz -C /opt/jdk/
-fi
 
 
 update-alternatives --install /usr/bin/java java /opt/jdk/jdk-10.0.1bin/java 1075
-update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk10.0.1/bin/javac 1075
+update-alternatives --install /usr/bin/javac javac /opt/jdk/jdk-10.0.1/bin/javac 1075
 
 
 
